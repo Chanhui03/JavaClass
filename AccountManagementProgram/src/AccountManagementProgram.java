3b27637f2dc788b23fd4aac7250a8b7463b70e9f
Chanhui03 <kw003004@gmail.com>
2025-11-18 16:25:59 +0900

    계좌 관리 프로그램 과제

--- AccountManagementProgram/src/AccountManagementProgram.java
+++ AccountManagementProgram/src/AccountManagementProgram.java
@@ -1,23 +1,33 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class AccountManagementProgram {
     static BankManager bm = new BankManager();
-    public static void main(String[] args) throws Exception {
-        // 계좌 관리 프로그램
-        Scanner scanner = new Scanner(System.in, "UTF-8");
+    static Scanner scanner = new Scanner(System.in, "UTF-8");
+    public static void main(String[] args) throws Exception { 
+        boolean running = true;       
         System.out.println("*** 한국해양은행에 오신 것을 환영합니다. ***");
-        // 계좌를 담을 해쉬맵
-
-        boolean running = true;
 
         while (running) { 
             int selectedMenu = getSelectedMenu(scanner);
             switch (selectedMenu) {
-                case 1:
-                case 2:
-                case 3:
-                case 4:
-                case 5:
+                case 1: // 입금
+                    processDeposit(scanner);
+                    break;
+                case 2: // 출금
+                    processWithdraw(scanner);
+                    break;
+                case 3: // 조회
+                    processInquiry(scanner);
+                    break;
+                case 4: // 전체 조회
+                    processInquiryAll();
+                    break;
+                case 5: // 종료
+                    System.out.println("프로그램을 종료합니다.");
+                    running = false;
+                    break;
+                default:
+                    break;
             }
             
         }
@@ -25,23 +35,49 @@ public class AccountManagementProgram {
         scanner.close();
     }
 
+    /**
+     * 메뉴 선택
+     * @param scanner
+     * @return 
+     */
     private static int getSelectedMenu(Scanner scanner) {
-        return 0;
+        System.out.print("입금:1, 출금:2, 조회:3, 전체 조회:4, 종료:5 >> ");
+        return scanner.nextInt();
     }
 
+    /**
+     * 
+     * @param scanner
+     */
     private static void processDeposit(Scanner scanner) {
+        System.out.print("계좌명과 액수 >> ");
+        String name = scanner.next();
+        int amount = scanner.nextInt();
+        bm.deposit(name, amount);
 
     }
 
     private static void processWithdraw(Scanner scanner) {
-
+        System.out.print("계좌명과 액수 >> ");
+        String name = scanner.next();
+        int amount = scanner.nextInt();
+        if (bm.withdraw(name, amount) < 0) {
+            System.out.println("잔액이 부족하여 출금할 수 없음!!");
+        }
     }
 
     private static void processInquiry(Scanner scanner) {
-
+        System.out.print("계좌명 >> ");
+        String name = scanner.next();
+        System.out.println(bm.getBalanceString(name));
     }
 
     private static void processInquiryAll() {
-        
+        String[] balances = bm.getAllBalanceString();
+        for (String s : balances) {
+            System.out.print(s + " ");
+        }
+        System.out.println();
     }
+    
 }
--- AccountManagementProgram/src/BankManager.java
+++ AccountManagementProgram/src/BankManager.java
@@ -3,37 +3,76 @@ import java.util.HashMap;
 public class BankManager {
     private HashMap<String, Integer> account;
 
+    // 생성자
     public BankManager() {
-
+        account = new HashMap<>();
     }
 
+    /**
+     * 입금처리 함수
+     * @param name 이름
+     * @param amount 잔액
+     */
     public void deposit(String name, int amount) {
-
+        // 계좌가 없으면 생성 후 초기화 
+        if (!account.containsKey(name)) 
+            account.put(name, 0);
+        // 계좌 잔액 업데이트
+        account.put(name, account.get(name) + amount);
     }
 
+    /**
+     * 출금처리 함수
+     * @param name 이름
+     * @param amount 잔액
+     * @return 남은 잔액
+     */
     public int withdraw(String name, int amount) {
+        // 계좌가 없을 시
+        if (!account.containsKey(name)) return -1;
 
-        return 0;
+        int balance = account.get(name);
+        if (balance < amount) return -1;
+        
+        balance -= amount;
+        account.put(name, balance);
+        return balance;
     }
 
+    /**
+     * 계좌명에 해당하는 잔고를 가져오는 함수
+     * @param name
+     * @return 잔고
+     */
     public int getBalance(String name) {
-
-        return 0;
+        return account.get(name);
     }
 
-    public String[] getAllAccounts() {
-
-        return null;
-    }
-
+    /**
+     * 계좌명에 해당하는 잔고를 "(계좌명:잔고액)" 형식의 문자열로 가져오는 함수
+     * @param name
+     * @return (계좌명:잔고액)
+     */
     public String getBalanceString(String name) {
+        if (!account.containsKey(name)) {
+            return "(" + name + ":계좌없음)";
+        }
 
-        return null;
+        return "(" + name + ":" + getBalance(name) + "원)";
     }
 
+    /**
+     * 모든 계좌의 잔고 정보를 문자열로 가져오는 함수
+     * @return 모든 (계좌명:잔고액)
+     */
     public String[] getAllBalanceString() {
+        String allAccountBalance[] = new String[account.size()];
+        int i = 0;
+        for (var entry : account.entrySet()) {
+            allAccountBalance[i++] = getBalanceString(entry.getKey());
+        }
 
-        return null;
+        return allAccountBalance;
     }
 
 }
